86613266cd47a17862d75bd1b3f9cfe76b79f791
Josh Correia <joshmcorreia@gmail.com>
2024-12-17 23:44:03 -0800

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,14 @@ impl PictureHanger {
     }
 
     fn half_frame_width(&self) -> f64 {
-        self.frame_width/2 as f64
+        self.frame_width / 2 as f64
     }
 
     fn outer_width(&self) -> f64 {
-        (self.wall_width - ((self.frame_width * self.num_pictures as f64) + ((self.num_pictures - 1) as f64 * self.distance_between_frames)))/2 as f64
+        (self.wall_width
+            - ((self.frame_width * self.num_pictures as f64)
+                + ((self.num_pictures - 1) as f64 * self.distance_between_frames)))
+            / 2 as f64
     }
 }
 
@@ -35,9 +38,15 @@ fn main() {
         wall_offset: settings.get("wall_offset").expect("Missing wall_offset"),
         num_pictures: settings.get("num_pictures").expect("Missing num_pictures"),
         frame_width: settings.get("frame_width").expect("Missing frame_width"),
-        height_from_nail_to_top_of_picture: settings.get("height_from_nail_to_top_of_picture").expect("Missing height_from_nail_to_top_of_picture"),
-        desired_top_of_picture_height: settings.get("desired_top_of_picture_height").expect("Missing desired_top_of_picture_height"),
-        distance_between_frames: settings.get("distance_between_frames").expect("Missing distance_between_frames"),
+        height_from_nail_to_top_of_picture: settings
+            .get("height_from_nail_to_top_of_picture")
+            .expect("Missing height_from_nail_to_top_of_picture"),
+        desired_top_of_picture_height: settings
+            .get("desired_top_of_picture_height")
+            .expect("Missing desired_top_of_picture_height"),
+        distance_between_frames: settings
+            .get("distance_between_frames")
+            .expect("Missing distance_between_frames"),
     };
 
     let height = picture_hanger.height();
@@ -50,12 +59,13 @@ fn main() {
     }
 
     let mut distance_from_wall: f64 = 0.0;
-    for painting in 1..picture_hanger.num_pictures+1 {
+    for painting in 1..picture_hanger.num_pictures + 1 {
         if painting == 1 {
             distance_from_wall = outer_width + picture_hanger.half_frame_width();
-        }
-        else {
-            distance_from_wall = distance_from_wall + picture_hanger.frame_width + picture_hanger.distance_between_frames;
+        } else {
+            distance_from_wall = distance_from_wall
+                + picture_hanger.frame_width
+                + picture_hanger.distance_between_frames;
         }
 
         let distance_from_wall_with_offset = distance_from_wall + picture_hanger.wall_offset;
@@ -63,6 +73,9 @@ fn main() {
         if painting != 1 {
             println!();
         }
-        println!("Painting {}:\nDistance from wall: {:.3}m\nDistance from ground: {:.3}m", painting, distance_from_wall_with_offset, height);
+        println!(
+            "Painting {}:\nDistance from wall: {:.3}m\nDistance from ground: {:.3}m",
+            painting, distance_from_wall_with_offset, height
+        );
     }
 }
